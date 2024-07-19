0d44ed31c0f5b86d1a42738e953d4e91e7f9fe1a
Vinicius Henrique <viniciuss.nuness@gmail.com>
2024-07-18 21:41:19 -0300

    criação de pacotes

--- java-anatomia-classes/src/edu/vinicius/primeirasemana/BoletimEstudantil.java
+++ java-anatomia-classes/src/edu/vinicius/primeirasemana/BoletimEstudantil.java
@@ -1,3 +1,4 @@
+package edu.vinicius.primeirasemana;
 public class BoletimEstudantil {
     public static void main(String[] args) {
         int mediaFinal = 7;
--- java-anatomia-classes/src/edu/vinicius/segundasemana/MinhaClasse.java
+++ java-anatomia-classes/src/edu/vinicius/segundasemana/MinhaClasse.java
@@ -1,3 +1,4 @@
+package edu.vinicius.segundasemana;
 public class MinhaClasse {
     
 public static void main (String [] args) {
