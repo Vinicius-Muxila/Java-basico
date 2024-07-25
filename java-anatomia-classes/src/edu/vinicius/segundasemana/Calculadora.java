4310150e4965d25d347fca4401802a62c12d44dd
Vinícius Henrique <120579281+Vinicius-Muxila@users.noreply.github.com>
2024-07-25 11:46:25 -0300

    complementando código calculadora

--- java-anatomia-classes/src/edu/vinicius/segundasemana/Calculadora.java
+++ java-anatomia-classes/src/edu/vinicius/segundasemana/Calculadora.java
@@ -10,15 +10,20 @@ package edu.vinicius.segundasemana;
  * @version 1.0
  * @since 23/07/2024
  */
-public class Calculadora {
-    /**
-     * Este método é utilizado para somar dois números inteiros
-     * @param numeroUm este é o primeiro parâmetro do método
-     * @param numeroDois este é o segundo parâmetro do método
-     * @return int o resultado deste método é a soma dos dois números.
-     *
-     */
-    public int somar(int numeroUm, int numeroDois) {
-        return numeroUm + numeroDois;
+public static void main (String [] args) {
+    public class Calculadora {
+        /**
+        * Este método é utilizado para somar dois números inteiros
+        * @param numeroUm este é o primeiro parâmetro do método
+        * @param numeroDois este é o segundo parâmetro do método
+        * @return int o resultado deste método é a soma dos dois números.
+        *
+        */
+
+            int numeroUm = 2;
+            int numeroDois = 5;
+        public int somar(int numeroUm, int numeroDois) {
+            return numeroUm + numeroDois;
+        }
     }
 }
